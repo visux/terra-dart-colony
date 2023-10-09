dc2da059b2f3e9297adc2acbff4cbd1f397e68dc
visux <francesco.lopez.u60d@gmail.com>
2023-10-09 23:42:03 +0200

    init

--- lib/src/Constants/Classic/ClassicHttpResources.dart
+++ lib/src/Constants/Classic/ClassicHttpResources.dart
@@ -1,6 +1,6 @@
 class ClassicHttpResources {
   static final String CLASSIC_COLUMBUS_MAINNET =
-      "https://lcd.terraclassic.community";
+      "https://lcd.terra.dev";
 
   static final String TERRA_REBELS_TESTNET = "https://lcd.hexxagon.dev";
 }
